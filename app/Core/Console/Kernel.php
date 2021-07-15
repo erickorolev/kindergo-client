915de0f648ab1feedd278fe37bba17e8de2f49d0
Sergey <sergey86@gmail.com>
2021-07-15 10:15:01 +0300

    Added domains core

---
+++ app/Core/Console/Kernel.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Parents\Commands\GetPortalVersionCommand;
+use Parents\Commands\ListActionsCommand;
+use Parents\Commands\ListTasksCommand;
+use Parents\Commands\SeedDeploymentDataCommand;
+use Parents\Commands\SeedTestingDataCommand;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        SeedDeploymentDataCommand::class,
+        SeedTestingDataCommand::class,
+        ListActionsCommand::class,
+        ListTasksCommand::class,
+        GetPortalVersionCommand::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')->hourly();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
---
+++ app/Core/Providers/AppServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\ServiceProvider;
+use Parents\Foundation\Portal;
+use Spatie\Fractal\Facades\Fractal;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->bind('Portal', function (): Portal {
+            return new Portal();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     * @psalm-suppress UndefinedMethod
+     * @psalm-suppress MixedReturnStatement
+     * @psalm-suppress MixedInferredReturnType
+     */
+    public function boot()
+    {
+        Fractal::macro('respondJsonApi', function (
+            int $statusCode = 200,
+            array $headers = []
+        ): JsonResponse {
+            return $this->respond($statusCode, array_merge($headers, [
+                'Content-Type' => 'application/vnd.api+json',
+            ]));
+        });
+    }
+}
