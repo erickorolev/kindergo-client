68b774484ac563a5a02ec1187f26bca9d126aea9
devwebok123 <dev.webok123@gmail.com>
2021-08-26 14:28:07 +0300

    refresh issue

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index');
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/{any}', function () {
+    return view('index');
+})->where('any', '.*');
 
 Route::middleware(['auth:sanctum', 'verified'])
     ->get('/admin', function () {
